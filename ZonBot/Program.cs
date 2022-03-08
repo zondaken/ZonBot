1cf91a7aaeb40ae824ac6c5fce083511fa6bb8bf
ZondaKeN <marino.zirks@gmail.com>
2022-03-08 04:14:28 +0100

    Update Program.cs

--- ZonBot/Program.cs
+++ ZonBot/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Reflection;
-using Discord;
+﻿using Discord;
 using Discord.Addons.Hosting;
 using Discord.Interactions;
 using Discord.WebSocket;
